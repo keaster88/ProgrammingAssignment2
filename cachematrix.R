394029bbafef99a1ecba93b79943dfdba044d00e
keaster88 <kayleighanna88@gmail.com>
2015-09-27 17:41:37 -0500

    Formatting Update

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,28 @@
-<<<<<<< HEAD
 ## Create a matrix object, caching the inverse mean
-
-=======
-## [Put comments here that describe what your functions do]
->>>>>>> master
 
 makeCacheMatrix <- function(x = matrix()) {
   t <- NULL
   set <- function(s) {
-    x <<- s
-    t <<- NULL
+      x <<- s
+      t <<- NULL
   }
   get <- function()
-    setinverse <- function(inverse) t <<- inverse
+  setinverse <- function(inverse) t <<- inverse
   getinverse <- function() t
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
-<<<<<<< HEAD
+
 ##returns cached matrix inverse or calculate matrix invervse if inverse is null
-=======
->>>>>>> master
+
 
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
   if(!is.null(i)){
-    message("getting cached data")
-    return(i)
+      message("getting cached data")
+      return(i)
   }
   data <- x$get()
   i <- solve(data, ...)
